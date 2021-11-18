8202a9c9b9ac6932a7608e835c7b340c98244912
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2021-11-18 12:21:10 +0000

    Project Creation and VR Import
    Added all prototype models to Unity Project and Imported the Steam VR package.

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float resetTime = 100005.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       //photocopier that overheats if on too long
+        
+       // gameObject.transform.position = gameObject.transform.position + new Vector3(Random.Range(-1.5f, 1.5f), 0, Random.Range(-1.5f, 1.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) 
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+        if(resetTime <= 0) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+        resetTime = resetTime - Time.deltaTime;
+    }
+}
---
+++ New Unity Project/Assets/lol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class lol : MonoBehaviour
+{
+    Rigidbody rb;
+    Collider col;
+    public bool connected = true;
+    public bool smashed = false;
+    public float vel = 0.1f;
+    float comVel = 0.0f;
+    Vector3 RelVel = Vector3.zero;
+    List<GameObject> gameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            connected = false; 
+        }
+        if (connected == false && smashed != true)
+        {
+            smashed = true;
+            rb = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+            rb.constraints = RigidbodyConstraints.None;
+            Instantiate(gameObject, transform.position, transform.rotation); 
+            Destroy(gameObject);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Rigidbody>())
+        {
+            RelVel = other.gameObject.GetComponent<Rigidbody>().velocity - gameObject.GetComponentInParent<Rigidbody>().velocity;
+            comVel = RelVel.x + RelVel.y + RelVel.z;
+            if (gameObject.tag != other.gameObject.tag && connected != false)
+            {
+                connected = false;
+            }
+        }
+    }
+}
+    
